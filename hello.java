f49bc071b08009f02cfa217f7dd1d802c184b640
lee <nikkiapril@hanmail.net>
2017-06-26 13:50:58 +0900

    create "hello world" program

---
+++ hello.java
@@ -0,0 +1,10 @@
+public class Hello{
+
+	public static void main(String[] args){
+	  System.out.println("Hello World");
+	  System.out.println("Hello world2")
+		  System.out.println("3")
+		  System.out.println("4)
+	}
+
+}
