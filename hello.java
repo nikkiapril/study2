75630b123cd459bd8f27a03be050bf0de3054888
lee <nikkiapril@hanmail.net>
2017-06-26 14:48:57 +0900

    added .'gitignore' file

--- hello.java
+++ hello.java
@@ -2,9 +2,10 @@ public class Hello{
 
 	public static void main(String[] args){
 	  System.out.println("Hello World");
-	  System.out.println("Hello world2")
-		  System.out.println("3")
-		  System.out.println("4)
+	  System.out.println("Hello world2");
+		  System.out.println("3");
+		  System.out.println("4");
+		  System.out.println("5");
 	}
 
 }
